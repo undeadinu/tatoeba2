17205cd09352a62abe95b72e7f841c775bece906
時流 <jiru@users.noreply.github.com>
2018-08-13 18:04:44 +0000

    Update database.php path

--- docs/cron/runner.sh
+++ docs/cron/runner.sh
@@ -3,8 +3,8 @@
 cmd="$1"
 shift 1
 
-DB_USER=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["login"];')
-DB_PASS=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["password"];')
-DB=$(php -r 'require "/var/www-prod/app/config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["database"];')
+DB_USER=$(php -r 'require "/var/www-prod/app/Config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["login"];')
+DB_PASS=$(php -r 'require "/var/www-prod/app/Config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["password"];')
+DB=$(php -r 'require "/var/www-prod/app/Config/database.php"; echo get_class_vars("DATABASE_CONFIG")["default"]["database"];')
 
 DB_PASS="$DB_PASS" DB_USER="$DB_USER" DB="$DB" "$cmd" "$@"
